dc8eff7d563c02e59d565eb2922ecd360d1f98ee
Vladimir Claveau <contact@delikreol.mq>
2026-07-07 17:49:01 +0000

    Updates

--- assets/chevron-up-BJ5-VOaP.js
+++
@@ -1,1 +0,0 @@
-import{V as e}from"./index-DAm3T75d.js";var t=e(`chevron-up`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t};
--- assets/plus-CetCPrsL.js
+++
@@ -1,1 +0,0 @@
-import{V as e}from"./index-DAm3T75d.js";var t=e(`plus`,[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`M12 5v14`,key:`s699le`}]]);export{t};
